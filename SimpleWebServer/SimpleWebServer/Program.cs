4d2e2b1c6f3243d54bbeab9d920961f47a92335d
Valeriy <valerii.ign@gmail.com>
2017-04-24 15:54:18 +0300

    Added some folder

---
+++ SimpleWebServer/SimpleWebServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server on port 8080");
+            HTTPServer server = new HTTPServer(8080);
+            server.Start();
+        }
+    }
+}
---
+++ SimpleWebServer/SimpleWebServer/Response.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SimpleWebServer
+{
+    public class Response
+    {
+        private Byte[] data = null;
+        private String status;
+        private String mime;
+
+        private Response(String status, String mime, Byte[] data)
+        {
+            this.status = status;
+            this.data = data;
+            this.mime = mime;
+        }
+
+        public static Response From(Request request)
+        {
+            if (request == null)
+                return MakeNullRequest();
+
+            if (request.Type == "GET")
+            {
+                String file = Environment.CurrentDirectory + HTTPServer.WEB_DIR + request.URL;
+                if (File.Exists(file))
+                {
+
+                }
+            }
+            else
+                return MakeMethodNotAllowed;
+        }
+
+        private static void MakeNullRequest()
+        {
+            String file = Environment.CurrentDirectory + HTTPServer.MSG_DIR + "400.html";
+            FileInfo fi = new FileInfo(file);
+            FileStream fs = fi.OpenRead();
+            BinaryReader reader = new BinaryReader(fs);
+            Byte[] d = new Byte[fs.Length];
+            reader.Read(d, 0, d.Length);
+            return new Response("400 Bad Request", "html/text", d);
+        }
+
+        private static void MakeMethodNotAllowed()
+        {
+            String file = Environment.CurrentDirectory + HTTPServer.MSG_DIR + "405.html";
+            FileInfo fi = new FileInfo(file);
+            FileStream fs = fi.OpenRead();
+            BinaryReader reader = new BinaryReader(fs);
+            Byte[] d = new Byte[fs.Length];
+            reader.Read(d, 0, d.Length);
+            return new Response("405 Method Not Allowed", "html/text", d);
+        }
+
+        private static void MakePageNotFound()
+        {
+            String file = Environment.CurrentDirectory + HTTPServer.MSG_DIR + "404.html";
+            FileInfo fi = new FileInfo(file);
+            FileStream fs = fi.OpenRead();
+            BinaryReader reader = new BinaryReader(fs);
+            Byte[] d = new Byte[fs.Length];
+            reader.Read(d, 0, d.Length);
+            return new Response("404 Page Not Found", "html/text", d);
+        }
+
+        public void Post(NetworkStream stream)
+        {
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine(String.Format("{0} {1}\r\nServer: {2}\r\nContent-Type: {3}\r\nAccept-Ranges: bytes\r\nContent-Length: {4}\r\n", 
+                HTTPServer.VERSION, status, HTTPServer.NAME, mime, data.Length));
+
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
