958436193cb8a3792442f6e3dd27aab8f5c47f90
Valeriy <valerii.ign@gmail.com>
2017-04-24 11:20:29 +0300

    first commit

---
+++ First/First/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, everyone!");
+            Console.ReadKey();
+        }
+    }
+}
